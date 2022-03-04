07c53918be90db442f7606520de798e9b7d77abe
Adrian Lee <adl0924@gmail.com>
2022-03-03 16:29:23 -0800

    removed strict validation

--- receiver/app.py
+++ receiver/app.py
@@ -66,7 +66,7 @@ def user_data(body):
     return NoContent, 201
 
 app = connexion.FlaskApp(__name__, specification_dir='') 
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True) 
+app.add_api("openapi.yaml") 
  
 if __name__ == "__main__": 
     app.run(port=8080)
