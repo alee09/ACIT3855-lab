2e8099e7af1d16349aca56f16f976c1b4ae47799
Adrian Lee <adl0924@gmail.com>
2022-03-31 16:54:33 -0700

    update

--- processing/app.py
+++ processing/app.py
@@ -109,7 +109,7 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='') 
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True) 
+app.add_api("openapi.yaml", strict_validation=False, validate_responses=False) 
  
 if __name__ == "__main__": 
     # run our standalone gevent server
